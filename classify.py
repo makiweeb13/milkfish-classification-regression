59aec97f0eb6b10f64288bf56e698e67417b1277
makiweeb13 <mariamaekiskis13@gmail.com>
2025-05-29 22:57:10 +0800

    implemented preprocessing and generated output

--- classify.py
+++ classify.py
@@ -1,12 +1,51 @@
+import os
+import cv2
 from data.loader import load_yolo_dataset
+from utils.image_utils import (
+    load_image,
+    crop_roi,
+    normalize_image,
+    denoise_image,
+    enhance_contrast
+)
 
 if __name__ == "__main__":
     images = "./dataset/train/images/"
     labels = "./dataset/train/labels/"
+    output_dir = "./outputs/fish_crops/"
+
+    os.makedirs(output_dir, exist_ok=True)
 
     df = load_yolo_dataset(images, labels)
 
+    print("Loaded DataFrame:")
     print(df.head())
 
     # Optionally save
     df.to_csv("./outputs/fish_size_dataframe.csv", index=False)
+
+    for index, row in df.iterrows():
+        image_path = os.path.join(images, row["image_id"] + ".jpg")
+
+        # Load the full image
+        img = load_image(image_path)
+        if img is None:
+            print(f"Could not load image {image_path}")
+            continue
+
+        # Crop the fish ROI
+        crop = crop_roi(img, row["bbox_x"], row["bbox_y"], row["bbox_width"], row["bbox_height"])
+
+        # Apply normalization, denoising, contrast enhancement
+        crop = normalize_image(crop)
+        crop = denoise_image(crop)
+        crop = enhance_contrast(crop)
+
+        # Save processed image crop
+        save_path = os.path.join(
+            output_dir,
+            f"{row['image_id']}_fish{row['fish_id']}_{row['mapped_class'].replace(' ', '')}.jpg"
+        )
+        cv2.imwrite(save_path, crop)
+
+    print("All fish crops processed and saved.")
